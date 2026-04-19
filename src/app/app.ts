40807ae602518952af49b5095dbc926cafc9058c
JoaoVittorOliveira <joaovittor324@gmail.com>
2026-04-19 18:59:08 -0300

    Modularizando a seção de "Projects"

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Hero } from './features/hero/hero';
+import { Projects } from './features/projects/projects';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, Hero],
+  imports: [CommonModule, Hero, Projects],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
