ab00d1fdc3a8ae10c729c097481d687fd2f5bd5d
JoaoVittorOliveira <joaovittor324@gmail.com>
2026-04-19 19:02:08 -0300

    Modularizando a seção "About"

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Hero } from './features/hero/hero';
 import { Projects } from './features/projects/projects';
+import { About } from './features/about/about';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, Hero, Projects],
+  imports: [CommonModule, Hero, Projects, About],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
