9d213b5b800b018b83a43aa7d66989d34281306b
JoaoVittorOliveira <joaovittor324@gmail.com>
2026-04-19 19:10:21 -0300

    Modularizando a seção "Approach"

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,11 +4,12 @@ import { Hero } from './features/hero/hero';
 import { Projects } from './features/projects/projects';
 import { About } from './features/about/about';
 import { Capabilities } from './features/capabilities/capabilities';
+import { Approach } from './features/approach/approach';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, Hero, Projects, About, Capabilities],
+  imports: [CommonModule, Hero, Projects, About, Capabilities, Approach],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
---
+++ src/app/features/approach/approach.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-approach',
+  imports: [],
+  templateUrl: './approach.html',
+  styleUrl: './approach.css',
+})
+export class Approach {}
