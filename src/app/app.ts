ad7fcef4a6064857cb9d2c1a8243b088d8f70a81
JoaoVittorOliveira <joaovittor324@gmail.com>
2026-04-19 18:52:55 -0300

    Modularização da seção "Hero"

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Hero } from './features/hero/hero';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, Hero],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
